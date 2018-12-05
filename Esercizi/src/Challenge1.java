389fba505555f384d69721e40e57d3ca2e5b6207
Santippello Forense <marco_santippe@hotmail.com>
2018-12-05 22:44:43 +0100

    enanche challenge

--- Esercizi/src/Challenge1.java
+++ Esercizi/src/Challenge1.java
@@ -1,52 +1,82 @@
 import java.util.Scanner;
-public class Challenge1 {
+public class Challenge {
 	
-	public static void main(String[] args) {
-		 
-		    //                             Challenge 1
-		    // Write a Java method to count all vowels in a string. 
-		    // Hint: Use substring() for this challenge question. You can also use charAt().
+	public static void main(String[] args) {		
+
+		int numTest = 0;
+		do {
+			System.out.println("Choose your test (1-5) , 0 To exit");
+			Scanner s = new Scanner(System.in);		
+			numTest = Integer.parseInt(s.next());
+			switch (numTest){
+				case (1) :
+					Challenge2();
+					break;
+				case (2) :
+					Challenge2();
+					break;
+				case (3) :
+					Challenge3();
+					break;
+				case (4) :
+					Challenge4();
+					break;
+				case (5) :
+					Challenge5();
+					break;
+			}			
+		}
+		while (numTest > 0);		
+	}
 		
-		// String word = "Deltaplano";
-		// int Vowels = 0;
-		// for (int i = 0; i < word.length(); i++) {
-		//     if (word.substring(i, i + 1).equals("a") 
-		//      || word.substring(i, i + 1).equals("e")
-		//      || word.substring(i, i + 1).equals("i") 
-		//      || word.substring(i, i + 1).equals("o")
-		//      || word.substring(i, i + 1).equals("u")) {
-		//         Vowels++;
-		//     }
-		// }
-		// System.out.println("The number of vowels in " + word + " is " + Vowels);}
-
+	private static void Challenge1(){
+		//                             Challenge 1
+		// Write a Java method to count all vowels in a string. 
+		// Hint: Use substring() for this challenge question. You can also use charAt().
+	
+		String word = "Deltaplano";
+		int Vowels = 0;
+		for (int i = 0; i < word.length(); i++) {
+			if (word.substring(i, i + 1).equals("a") 
+				|| word.substring(i, i + 1).equals("e")
+				|| word.substring(i, i + 1).equals("i") 
+				|| word.substring(i, i + 1).equals("o")
+				|| word.substring(i, i + 1).equals("u")) {
+				Vowels++;
+			}
+		}
+		System.out.println("The number of vowels in " + word + " is " + Vowels);
+	}			
+	
+	private static void Challenge2(){
 		//**********************************************************************************	
-        //                                 Challenge 2
+		//                                 Challenge 2
 
-        // Write a Java method to count all words in a string entered by a user. 
+		// Write a Java method to count all words in a string entered by a user. 
 		// Assume that each word is separated by a 
 		// SINGLE SPACE and that there is at least 1 word in the string. 
 
-        // Example: "This is a big world" has 4 words
+		// Example: "This is a big world" has 4 words
 
 		
-		// String parola = "Sono nato in italia";
-		// int conta = 1;
-		// for(int i = 0; i<parola.length(); i++) {
-		// 	if(parola.substring(i, i+1).equals(" ")) {
-		// 		conta++;
-		// System.out.println("The number of words in '" + parola + "' is " + conta);
-		// 	}
-		// }
-		
-	    // ********************************************************************************
+		String parola = "Sono nato in italia";
+		int conta = 1;
+		for(int i = 0; i<parola.length(); i++) {
+			if(parola.substring(i, i+1).equals(" ")) {
+				conta++;
+		System.out.println("The number of words in '" + parola + "' is " + conta);
+			}
+		}
+	}
+	private static void Challenge3(){
+		// ********************************************************************************
 		//                                 Challenge3
 		
 		// Write a Java method to check whether a string entered by a user is a valid password.
-        // Password rules:
+		// Password rules:
 
-        // A password must have at least ten characters.
-        // A password must have at least one uppercase letter.
+		// A password must have at least ten characters.
+		// A password must have at least one uppercase letter.
 		// A password must contain at least two digits.
 		// Note: Password MUST meet all 3 rules to be considered valid.
 
@@ -73,23 +103,23 @@ public class Challenge1 {
 			
 			if (password.substring(i, i + 1).equals("1")
 					
-			   ||  password.substring(i, i + 1).equals("2")
+				||  password.substring(i, i + 1).equals("2")
 			
-			   ||  password.substring(i, i + 1).equals("3")
+				||  password.substring(i, i + 1).equals("3")
 			
-			   ||  password.substring(i, i + 1).equals("4")
+				||  password.substring(i, i + 1).equals("4")
 			
-			   ||  password.substring(i, i + 1).equals("5")
+				||  password.substring(i, i + 1).equals("5")
 			
-			   ||  password.substring(i, i + 1).equals("6")
+				||  password.substring(i, i + 1).equals("6")
 			
-			   ||  password.substring(i, i + 1).equals("7")
+				||  password.substring(i, i + 1).equals("7")
 			
-			   ||  password.substring(i, i + 1).equals("8")
+				||  password.substring(i, i + 1).equals("8")
 			
-			   ||  password.substring(i, i + 1).equals("9")
+				||  password.substring(i, i + 1).equals("9")
 			
-			   ||  password.substring(i, i + 1).equals("0")
+				||  password.substring(i, i + 1).equals("0")
 			
 			) {
 				
@@ -97,28 +127,27 @@ public class Challenge1 {
 				
 			}
 		}
-			
-			if(digits < 2) {
-				
-				validPassword = false;
-				
-			}
-			
-			if(validPassword) {
-				
-				System.out.println("The password is valid");
-				
-			}
-			
-			else {
-				
-				System.out.println("The password is invalid");
-				
-			}
-			
 		
-			}
+		if(digits < 2) {
+			
+			validPassword = false;
+			
+		}
 		
+		if(validPassword) {
+			
+			System.out.println("The password is valid");
+			
+		}
+		
+		else {
+			
+			System.out.println("The password is invalid");
+			
+		}
+	}
+	
+	private static void Challenge4(){
 		//*********************************************************************************
 		//                                Challenge4
 		
@@ -144,33 +173,32 @@ public class Challenge1 {
 		// else {
 		// 	System.out.println("Yellow");
 		// }
-		
+	}
+	private static void Challenge5(){
 		// ***********************************************************************************
-		//                                   Challenge 5
-		
+		//                                   Challenge 5			
 		//  Write a Java program to find the common elements between two arrays (int values) 
 		//  and print the similar elements to the console. 
 		//  Assume that both arrays have the same length (equal number of int elements)
-        //  For example, for:
+		//  For example, for:
+		int[] array1 = {2, 3, 4};
 
-        //  int[] array1 = {2, 3, 4};
+		int[] array2 = {3, 6,7};
 
-        //  int[] array2 = {3, 6,7};
+			//  3 is printed out to the console
 
-        //  3 is printed out to the console
-
-		// int[] array1 = {3, 33, 44, 66, 77, 99};
-		// int[] array2 = {33, 45, 54, 64, 99, 8};
+		//int[] array1 = {3, 33, 44, 66, 77, 99};
+		//int[] array2 = {33, 45, 54, 64, 99, 8};
 		
-		// for(int x = 0; x<array1.length; x++) {
-		// 	for(int y = 0; y<array2.length; y++) {
-		// 		if(array1[x] == array2[y]) {
-		// 			System.out.println(array2[y]);
-		// 		}
-		// 	}
+		for(int x = 0; x<array1.length; x++) {
+			for(int y = 0; y<array2.length; y++) {
+				if(array1[x] == array2[y]) {
+					System.out.println(array2[y]);
+				}
+			}
 			
-		// }				
-			
+		}	
+	}
 }
 		
 		
